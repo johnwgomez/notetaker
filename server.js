cba95f4e235cc7fd1c8eb2c3b7f0fc057cc75cb5
johnwgomez <johnwgomez10@gmail.com>
2024-09-14 21:25:45 -0700

    psuedo code what i needed from server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+//1. i need create dependencies and create unique ids
+const express = require('express');
+const
+
+// 2. Set up Middleware
+
+// 3. HTML Routes
+
+// 4. API Routes needed (GET, POST, DELETE requests)
