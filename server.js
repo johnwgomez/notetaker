d80e8bb691626afaf0d595154517e9d32fe980c5
johnwgomez <johnwgomez10@gmail.com>
2024-09-15 12:52:08 -0700

    html routes complete

--- server.js
+++ server.js
@@ -7,8 +7,18 @@ const { v4: uuidv4 } = require('uuid'); // unique IDs
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// 2. Set up Middleware
+// 2. Set up Middleware | Data Parse
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 // 3. HTML Routes
-
+// notes return to notes.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+  });
+// get *return to index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
 // 4. API Routes needed (GET, POST, DELETE requests)
